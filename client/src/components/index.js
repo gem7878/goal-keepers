1f495ad7c8cc6532750103e201f218e5e0f2c8bd
Kim MinJeong <100185951+dev-minjeong@users.noreply.github.com>
2023-12-14 16:00:53 +0900

    Revert "전반적인 레이아웃 퍼블리싱"

--- client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from "./Navbar.tsx";
-export { default as MyGoals } from "./MyGoals.tsx";
-export { default as CreateButton } from "./CreateButton.tsx";
-export { default as PostBox } from "./PostBox.tsx";
-export { default as Modal } from "./Modal.tsx";
