692c562eb4b62b4789f8c1f5bad9cb8f8bee195f
dev-minjeong <vitaminjeong1026@gmail.com>
2023-12-23 03:33:01 +0900

    회원가입 유효성 검사 완료

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -1,133 +1,110 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
   handleConfirmEmail,
   handleConfirmNickName,
   handleSubmitForm,
-} from "./actions";
-import shortid from "shortid";
-import { useRouter } from "next/navigation";
+} from './actions';
+import shortid from 'shortid';
+import { useRouter } from 'next/navigation';
+import { Controller, SubmitHandler, useForm, useWatch } from 'react-hook-form';
+
+interface IFormInput {
+  email: string;
+  password: string;
+  passwordCheck: string;
+  nickname: string;
+}
 
 const Register = () => {
-  const [form, setForm] = useState({
-    email: "",
-    validEmail: false,
-    nonEmailDuplication: false,
-    pw: "",
-    validPw: "",
-    pwCheck: "",
-    correctPwCheck: false,
-    nickname: "",
-    nonNicknameDuplication: false,
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+    getValues,
+    setError,
+    watch,
+  } = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordCheck: '',
+      nickname: '',
+    },
   });
-  const [message, setMessage] = useState({
-    email: "",
-    pw: "",
-    pwCheck: "",
-    nickname: "",
-  });
-  const [messageGreen, setMessageGreen] = useState({
+  const [isCheckingDuplicate, setIsCheckingDuplicate] = useState({
     email: false,
-    pw: false,
-    pwCheck: false,
     nickname: false,
   });
 
   const router = useRouter();
 
-  const test = shortid.generate();
-  const messageList = [
-    "* 이메일 중복을 확인해주세요",
-    "* 이메일을 형식에 맞게 입력해주세요.",
-    "* 비밀번호를 형식에 맞게 입력해주세요.",
-  ];
+  // 유효성 검사
+  const EMAIL_REGEX =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,20}$/;
 
-  useEffect(() => {
-    if (form.pwCheck.length > 0) {
-      form.correctPwCheck
-        ? setMessage({ ...message, pwCheck: "" })
-        : setMessage({
-            ...message,
-            pwCheck: "* 비밀번호가 일치하지 않습니다.",
-          });
-      setMessageGreen({ ...messageGreen, pwCheck: false });
-    }
-  }, [form.correctPwCheck, form.pwCheck]);
-
-  const hadleInputChange = (
-    type: string,
-    e: React.FormEvent<HTMLInputElement>
-  ) => {
-    if (type === "email") {
-      setForm({
-        ...form,
-        email: e.currentTarget.value,
-        nonEmailDuplication: false,
-      });
-    } else if (type === "pw") {
-      setForm({
-        ...form,
-        pw: e.currentTarget.value,
-        correctPwCheck: e.currentTarget.value === form.pwCheck,
-      });
-    } else if (type === "pwCheck") {
-      setForm({
-        ...form,
-        pwCheck: e.currentTarget.value,
-        correctPwCheck: e.currentTarget.value === form.pw,
-      });
-    } else if (type === "nickname") {
-      setForm({
-        ...form,
-        nickname: e.currentTarget.value,
-        nonNicknameDuplication: false,
-      });
-    }
-  };
-
+  // 이메일 중복 확인
   const handleConfirmDuplicationEmail = async () => {
-    await handleConfirmEmail(form.email)
-      .then((response) => {
-        setMessage({ ...message, email: `* ${response.message}` });
-        setMessageGreen({ ...messageGreen, email: response.success });
-        setForm({ ...form, nonEmailDuplication: true });
-      })
-      .catch((error) => console.log(error));
-  };
-  const handleConfirmDuplicationNickname = async () => {
-    await handleConfirmNickName(form.nickname)
-      .then((response) => {
-        setMessage({ ...message, nickname: `* ${response.message}` });
-        setMessageGreen({ ...messageGreen, nickname: response.success });
-        setForm({ ...form, nonNicknameDuplication: true });
-      })
-      .catch((error) => console.log(error));
-  };
-  const hadleConfirmForm = async () => {
-    if (
-      form.email &&
-      // form.validEmail &&
-      form.nonEmailDuplication &&
-      form.pw &&
-      // form.validPw &&
-      form.pwCheck &&
-      form.correctPwCheck &&
-      form.nonNicknameDuplication &&
-      form.nickname
-    ) {
-      await handleSubmitForm({
-        email: form.email,
-        password: form.pw,
-        nickname: form.nickname,
-      })
+    const emailValue = getValues('email');
+    if (emailValue.length > 0) {
+      await handleConfirmEmail(emailValue)
         .then((response) => {
-          alert(`${response.nickname}님! 회원가입이 완료되었습니다.`);
-          return router.push("/login");
+          setIsCheckingDuplicate({
+            ...isCheckingDuplicate,
+            email: response.success,
+          });
+          alert(response.message);
         })
         .catch((error) => console.log(error));
     } else {
-      alert("다시 확인해주세요.");
+      setError('email', {
+        type: 'required',
+      });
+    }
+  };
+  // 닉네임 중복 확인
+  const handleConfirmDuplicationNickname = async () => {
+    const nicknameValue = getValues('nickname');
+    if (nicknameValue.length > 0) {
+      await handleConfirmNickName(nicknameValue)
+        .then((response) => {
+          setIsCheckingDuplicate({
+            ...isCheckingDuplicate,
+            nickname: response.success,
+          });
+          alert(response.message);
+        })
+        .catch((error) => console.log(error));
+    } else {
+      setError('nickname', {
+        message: '닉네임을 입력하세요.',
+      });
+    }
+  };
+
+  // 회원가입
+  const hadleConfirmForm: SubmitHandler<IFormInput> = async (data) => {
+    const NICK_NAME =
+      data.nickname.length > 0 ? data.nickname : shortid.generate();
+
+    if (!isCheckingDuplicate.email) {
+      alert('이메일 중복여부를 체크해주세요.');
+    } else if (!isCheckingDuplicate.nickname && data.nickname.length > 0) {
+      alert('닉네임 중복여부를 체크해주세요.');
+    } else {
+      await handleSubmitForm({
+        email: data.email,
+        password: data.password,
+        nickname: NICK_NAME,
+      })
+        .then((response) => {
+          alert(`${response.nickname}님! 회원가입이 완료되었습니다.`);
+          return router.push('/login');
+        })
+        .catch((error) => console.log(error));
     }
   };
 
@@ -135,45 +112,60 @@ const Register = () => {
     <>
       <h2 className="w-full text-center text-xl mt-20 font-bold">회원가입</h2>
       <form
-        action={hadleConfirmForm}
+        onSubmit={handleSubmit(hadleConfirmForm)}
         className="flex flex-col w-full gap-4"
         id="register"
       >
         <li className="flex flex-col w-full">
           <div className="flex items-end">
             <label htmlFor="email_input" className="text-sm">
-              이메일
+              이메일<span className="text-orange-400">*</span>
             </label>
             <p
               className={`${
-                messageGreen.email ? "text-green-600" : "text-red-600"
+                errors?.email ? 'text-red-600' : 'text-green-600'
               } text-xs ml-2`}
             >
-              {message.email}
+              {errors?.email?.type === 'required'
+                ? '이메일을 입력해주세요.'
+                : errors?.email?.type === 'pattern'
+                ? '이메일 양식에 맞게 입력해주세요.'
+                : ''}
             </p>
           </div>
           <div className="border-b w-full flex justify-between p-3">
-            <input
-              id="email_input"
-              type="email"
-              placeholder="이메일을 입력하세요"
-              className="w-[calc(100%-52px)]"
-              value={form.email}
-              onChange={(e) => hadleInputChange("email", e)}
-            ></input>
+            <Controller
+              name="email"
+              control={control}
+              rules={{
+                required: true,
+                pattern: EMAIL_REGEX,
+                onChange: () =>
+                  setIsCheckingDuplicate({
+                    ...isCheckingDuplicate,
+                    email: false,
+                  }),
+              }}
+              render={({ field }) => (
+                <input
+                  type="email"
+                  className="w-[calc(100%-52px)]"
+                  {...field}
+                  placeholder="이메일을 입력하세요"
+                ></input>
+              )}
+            ></Controller>
             <button
               className={`${
-                form.nonEmailDuplication ? "text-gray-400" : "text-orange-400"
+                !isCheckingDuplicate.email && !errors?.email
+                  ? 'text-orange-400'
+                  : 'text-gray-400'
               } text-sm text-center w-14`}
               type="button"
-              onClick={() => {
-                if (form.email.length > 0) {
-                  handleConfirmDuplicationEmail();
-                } else {
-                  setMessage({ ...message, email: "* 이메일을 입력하세요" });
-                  setMessageGreen({ ...messageGreen, email: false });
-                }
-              }}
+              onClick={() => handleConfirmDuplicationEmail()}
+              disabled={
+                isCheckingDuplicate.email || errors?.email?.type === 'pattern'
+              }
             >
               중복확인
             </button>
@@ -181,80 +173,106 @@ const Register = () => {
         </li>
         <li className="flex flex-col w-full">
           <div className="flex items-end">
-            <label htmlFor="password_input" className="text-sm">
-              비밀번호
+            <label htmlFor="password_input" className="text-sm w-24">
+              비밀번호<span className="text-orange-400">*</span>
             </label>
-            <p className="text-red-600 text-xs ml-2">{message.pw}</p>
+            <p className="text-red-600 text-xs ml-2">
+              {errors?.password?.type === 'required'
+                ? '비밀번호를 입력해주세요.'
+                : errors?.password?.type === 'pattern'
+                ? '8~20자의 소문자 영단어, 숫자, 특수문자를 모두 포함한 비밀번호를 입력해주세요.'
+                : ''}
+            </p>
           </div>
           <div className="border-b w-full flex justify-between p-3">
-            <input
-              id="password_input"
-              type="password"
-              className="w-full"
-              placeholder="비밀번호를 입력하세요"
-              value={form.pw}
-              onChange={(e) => hadleInputChange("pw", e)}
-            ></input>
+            <Controller
+              name="password"
+              control={control}
+              rules={{
+                required: true,
+                pattern: PASSWORD_REGEX,
+              }}
+              render={({ field }) => (
+                <input
+                  type="password"
+                  className="w-full"
+                  {...field}
+                  placeholder="비밀번호를 입력하세요"
+                ></input>
+              )}
+            ></Controller>
           </div>
         </li>
-        <li className="flex flex-col w-full">
-          <div className="flex items-end">
-            <label htmlFor="password-check_input" className="text-sm">
-              비밀번호 재확인
-            </label>
-            <p className="text-red-600 text-xs ml-2">{message.pwCheck}</p>
-          </div>
-          <div className="border-b w-full flex justify-between p-3">
-            <input
-              id="password-check_input"
-              type="password"
-              className="w-full"
-              placeholder="비밀번호를 입력하세요"
-              value={form.pwCheck}
-              onChange={(e) => hadleInputChange("pwCheck", e)}
-            ></input>
-          </div>
-        </li>
+        <Controller
+          name="passwordCheck"
+          control={control}
+          rules={{
+            required: true,
+            validate: {
+              check: (val) => {
+                if (getValues('password') !== val) {
+                  return '비밀번호가 일치하지 않습니다.';
+                }
+              },
+            },
+          }}
+          render={({ field, fieldState }) => (
+            <li className="flex flex-col w-full">
+              <div className="flex items-end">
+                <label htmlFor="password-check_input" className="text-sm">
+                  비밀번호 재확인<span className="text-orange-400">*</span>
+                </label>
+                <p className="text-red-600 text-xs ml-2">
+                  {fieldState?.error ? fieldState?.error.message : ''}
+                </p>
+              </div>
+              <div className="border-b w-full flex justify-between p-3">
+                <input
+                  type="password"
+                  className="w-full"
+                  {...field}
+                  placeholder="비밀번호를 입력하세요"
+                ></input>
+              </div>
+            </li>
+          )}
+        ></Controller>
         <li className="flex flex-col w-full">
           <div className="flex items-end">
             <label htmlFor="nickname_input" className="text-sm">
               닉네임
             </label>
-            <p
-              className={`${
-                messageGreen.nickname ? "text-green-600" : "text-red-600"
-              } text-xs ml-2`}
-            >
-              {message.nickname}
-            </p>
           </div>
           <div className="border-b w-full flex justify-between p-3">
-            <input
-              id="nickname_input"
-              type="text"
-              className="w-[calc(100%-52px)]"
-              placeholder="닉네임을 입력하세요(15자 이하)"
-              value={form.nickname}
-              onChange={(e) => hadleInputChange("nickname", e)}
-            ></input>
+            <Controller
+              name="nickname"
+              control={control}
+              rules={{
+                maxLength: 15,
+                onChange: () =>
+                  setIsCheckingDuplicate({
+                    ...isCheckingDuplicate,
+                    nickname: false,
+                  }),
+              }}
+              render={({ field }) => (
+                <input
+                  type="text"
+                  className="w-[calc(100%-52px)]"
+                  {...field}
+                  placeholder="닉네임을 입력하세요(15자 이하)"
+                ></input>
+              )}
+            ></Controller>
             <button
               className={`${
-                form.nonNicknameDuplication
-                  ? "text-gray-400"
-                  : "text-orange-400"
+                !isCheckingDuplicate.nickname
+                  ? 'text-orange-400'
+                  : 'text-gray-400'
               } text-sm text-center w-14`}
               type="button"
-              onClick={() => {
-                if (form.nickname.length > 0) {
-                  handleConfirmDuplicationNickname();
-                } else {
-                  setMessage({
-                    ...message,
-                    nickname: "* 닉네임을 입력하세요",
-                  });
-                  setMessageGreen({ ...messageGreen, nickname: false });
-                }
-              }}
+              onClick={() => handleConfirmDuplicationNickname()}
+              disabled={isCheckingDuplicate.nickname}
             >
               중복확인
             </button>
@@ -263,26 +281,15 @@ const Register = () => {
       </form>
       <input
         type="submit"
-        value={"완료"}
-        className={`gk-primary-login-button`}
+        value={'완료'}
         disabled={
-          !(
-            form.email.length > 0 &&
-            form.pw.length > 0 &&
-            form.pwCheck.length > 0 &&
-            form.nickname.length > 0 &&
-            form.correctPwCheck &&
-            form.nonEmailDuplication &&
-            form.nonNicknameDuplication
-          )
-          // !(
-          //   form.validEmail &&
-          //   form.nonEmailDuplication &&
-          //   form.validPw &&
-          //   form.correctPwCheck &&
-          //   form.nonNicknameDuplication
-          // )
+          isValid === false ||
+          isCheckingDuplicate.email === false ||
+          (watch('nickname').length > 0
+            ? isCheckingDuplicate.nickname === false
+            : false)
         }
+        className={`gk-primary-login-button`}
         form="register"
       ></input>
     </>
