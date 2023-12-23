458833b0991459f00a5bbeabb5fdb3b6f5013290
pieceofjc <pieceofjc@gmail.com>
2023-12-23 18:59:47 +0900

    Feat: Member Api Exception

--- server/src/main/java/com/goalkeepers/server/dto/ChangePasswordRequestDto.java
+++ server/src/main/java/com/goalkeepers/server/dto/ChangePasswordRequestDto.java
@@ -1,5 +1,7 @@
 package com.goalkeepers.server.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChangePasswordRequestDto {
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올바른 이메일 형식으로 작성해주세요.")
     private String email;
+
+    @NotBlank(message = "이전 비밀번호를 입력해주세요.")
     private String exPassword;
+
+    @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
     private String newPassword;
 }
