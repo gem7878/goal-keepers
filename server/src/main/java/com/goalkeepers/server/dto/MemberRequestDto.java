4d5552beee12a29628a8379dbfc295a7b63d9e11
pieceofjc <pieceofjc@gmail.com>
2023-12-23 16:35:18 +0900

    Feat: exception handler

--- server/src/main/java/com/goalkeepers/server/dto/MemberRequestDto.java
+++ server/src/main/java/com/goalkeepers/server/dto/MemberRequestDto.java
@@ -1,9 +1,17 @@
 package com.goalkeepers.server.dto;
 
+import javax.swing.Spring;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.goalkeepers.server.entity.Role;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 import com.goalkeepers.server.entity.Member;
 
 import lombok.AllArgsConstructor;
@@ -16,8 +24,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class MemberRequestDto {
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올바른 이메일 형식으로 작성해주세요.")
     private String email;
+    
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
+
+    @Max(value = 15, message = "닉네임은 3 ~ 15자 입니다.")
+    @Min(value = 3, message = "닉네임은 3 ~ 15자 입니다.")
     private String nickname;
 
     public Member toMember(PasswordEncoder passwordEncoder) {
