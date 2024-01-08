eec11a6ebf00245174454808e927a77f00234f29
pieceofjc <pieceofjc@gmail.com>
2024-01-08 14:51:47 +0900

    Fix: Post api에서 이미지 부분 수정

--- server/src/main/java/com/goalkeepers/server/repository/PostRepositoryImpl.java
+++ server/src/main/java/com/goalkeepers/server/repository/PostRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.goalkeepers.server.repository;
 
+import java.util.Objects;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -13,6 +14,7 @@ import com.goalkeepers.server.config.SecurityUtil;
 import com.goalkeepers.server.dto.PostListPageResponseDto;
 import com.goalkeepers.server.entity.Member;
 import com.goalkeepers.server.entity.Post;
+import com.goalkeepers.server.service.FirebaseStorageService;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import static com.goalkeepers.server.entity.QPost.post;
@@ -26,6 +28,7 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
     private final MemberRepository memberRepository;
     private final PostLikeRepository likeRepository;
     private final GoalShareRepository shareRepository;
+    private final FirebaseStorageService firebaseStorageService;
 
     @Override
     public Page<PostListPageResponseDto> searchAll(Pageable pageable) {
@@ -38,25 +41,20 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
                             .fetch();
         
         Long memberId = SecurityUtil.getCurrentMemberId();
-        List<PostListPageResponseDto> page;
-
-        if (memberId == null) {
-            page = posts
-                    .stream()
-                    .map(post -> PostListPageResponseDto.of(post, false, false))
-                    .collect(Collectors.toList());
-        } else {
-            Optional<Member> member = memberRepository.findById(memberId);
-            page = posts
-                    .stream()
-                    .map(post -> {
-                        boolean isLike = likeRepository.existsByMemberAndPost(member.get(), post);
-                        boolean isShare = post.getGoal() != null ? shareRepository.existsByMemberAndGoal(member.get(), post.getGoal()) : false;
-                        return PostListPageResponseDto.of(post, isLike, isShare);
-                    })
-                    .collect(Collectors.toList());
-        }
-        
+        List<PostListPageResponseDto> page = posts.stream().map(post -> {
+            boolean isLike = false;
+            boolean isShare = false;
+            String imageUrl = null;
+            if (Objects.nonNull(post.getGoal()) && Objects.nonNull(post.getGoal().getImageUrl())) {
+                imageUrl = firebaseStorageService.showFile(post.getGoal().getImageUrl());
+                if (Objects.nonNull(memberId)) {
+                    Optional<Member> member = memberRepository.findById(memberId);
+                    isLike = likeRepository.existsByMemberAndPost(member.get(), post);
+                    isShare = shareRepository.existsByMemberAndGoal(member.get(), post.getGoal());
+                }
+            }
+            return PostListPageResponseDto.of(post, imageUrl, isLike, isShare);
+        }).collect(Collectors.toList());
         
         int totalSize = queryFactory
                         .selectFrom(post)
@@ -81,8 +79,11 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
                     .stream()
                     .map(post -> {
                         boolean isLike = likeRepository.existsByMemberAndPost(member, post);
-                        boolean isShare = post.getGoal() != null ? shareRepository.existsByMemberAndGoal(member, post.getGoal()) : false;
-                        return PostListPageResponseDto.of(post, isLike, isShare);
+                        String imageUrl = null;
+                        if(Objects.nonNull(post.getGoal()) && Objects.nonNull(post.getGoal().getImageUrl())) {
+                            imageUrl = firebaseStorageService.showFile(post.getGoal().getImageUrl());
+                        }
+                        return PostListPageResponseDto.of(post, imageUrl, isLike, false);
                     })
                     .collect(Collectors.toList());
         
--- server/src/main/java/com/goalkeepers/server/service/BoardService.java
+++ server/src/main/java/com/goalkeepers/server/service/BoardService.java
@@ -1,6 +1,6 @@
 package com.goalkeepers.server.service;
 
-import java.util.List;
+import java.util.Objects;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -12,10 +12,8 @@ import com.goalkeepers.server.dto.PostResponseDto;
 import com.goalkeepers.server.entity.Goal;
 import com.goalkeepers.server.entity.Member;
 import com.goalkeepers.server.entity.Post;
-import com.goalkeepers.server.entity.PostLike;
 import com.goalkeepers.server.repository.GoalRepository;
 import com.goalkeepers.server.repository.MemberRepository;
-import com.goalkeepers.server.repository.PostLikeRepository;
 import com.goalkeepers.server.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -27,44 +25,54 @@ public class BoardService extends CommonService {
     private final PostRepository postRepository;
     private final GoalRepository goalRepository;
     private final MemberRepository memberRepository;
-    private final PostLikeRepository likeRepository;
+    private final LikeShareService likeShareService;
+    private final FirebaseStorageService firebaseStorageService;
 
-    // 모든 게시글 보기
+    /*
+     * 모든 게시글 가져오기
+     * 나의 모든 게시글 가져오기
+     */
+
     public Page<PostListPageResponseDto> getAllPostList(int pageNumber) {
         return postRepository.searchAll(PageRequest.of(pageNumber - 1, 10));
     }
 
-    // 나의 모든 게시글 보기
     public Page<PostListPageResponseDto> getMyAllPostList(int pageNumber) {
         Member member = isMemberCurrent(memberRepository);
         return postRepository.searchMyAllPost(PageRequest.of(pageNumber - 1, 10), member);
     }
 
     /*
-     *  게시글 쓰기
-        게시글 수정하기
-        게시글 삭제하기
+     * 게시글 쓰기
+     * 게시글 수정하기
+     * 게시글 삭제하기
      */
 
     public PostResponseDto createMyPost(PostRequestDto requestDto) {
         Member member = isMemberCurrent(memberRepository);
         Goal goal = isMyGoal(memberRepository, goalRepository, requestDto.getGoalId());
         Post post = requestDto.toPost(member, goal);
-        return PostResponseDto.of(postRepository.save(post));     
+        String imageUrl = goal.getImageUrl();
+        if(Objects.nonNull(imageUrl) && !imageUrl.isEmpty()) {
+            imageUrl = firebaseStorageService.showFile(imageUrl);
+        }
+        return PostResponseDto.of(postRepository.save(post), imageUrl);     
     }
 
-    public PostResponseDto updateMyPost(PostRequestDto requestDto, Long postId) {  
-        return PostResponseDto.of(Post.postUpdate(isMyPost(memberRepository, postRepository, postId), requestDto));
+    public PostResponseDto updateMyPost(PostRequestDto requestDto, Long postId) {
+        Post post = Post.postUpdate(isMyPost(memberRepository, postRepository, postId), requestDto);
+        Goal goal = post.getGoal();
+        String imageUrl = Objects.nonNull(goal)? goal.getImageUrl() : null;
+        if(Objects.nonNull(imageUrl) && !imageUrl.isEmpty()) {
+            imageUrl = firebaseStorageService.showFile(imageUrl);
+        }
+        return PostResponseDto.of(post, imageUrl);
     }
 
     public void deleteMyPost(Long postId) {
         Post post = isMyPost(memberRepository, postRepository, postId);
-        
-        List<PostLike> likeList = likeRepository.findAllByPost(post);
-        for (PostLike like : likeList) {
-            likeRepository.delete(like);
-        }
-        postRepository.delete(isMyPost(memberRepository, postRepository, postId));
+        likeShareService.deleteLike(post);
+        postRepository.delete(post);
     }
 }
 
