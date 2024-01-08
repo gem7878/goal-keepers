bd81185ac2c261bcf005e5eeffb93aac6e4e9172
pieceofjc <pieceofjc@gmail.com>
2024-01-08 19:24:47 +0900

    Fix: create goal 디폴트 값 수정

--- server/src/main/java/com/goalkeepers/server/dto/GoalRequestDto.java
+++ server/src/main/java/com/goalkeepers/server/dto/GoalRequestDto.java
@@ -1,5 +1,6 @@
 package com.goalkeepers.server.dto;
 
+import java.util.Objects;
 import java.time.LocalDate;
 
 import com.goalkeepers.server.entity.Goal;
@@ -21,20 +22,20 @@ public class GoalRequestDto {
     private String title;
 
     // @NotBlank(message = "설명을 입력해주세요.")
-    private String description = "";
+    private String description;
 
     // @NotNull(message = "시작 날짜를 입력해주세요.")
-    private LocalDate startDate = LocalDate.now();
+    private LocalDate startDate;
 
     // @NotNull(message = "끝나는 날짜를 입력해주세요.")
-    private LocalDate endDate = LocalDate.now().plusYears(1);
+    private LocalDate endDate;
 
     public Goal toGoal(Member member, String imageUrl) {
         return Goal.builder()
                 .title(title)
-                .description(description)
-                .startDate(startDate)
-                .endDate(endDate)
+                .description(Objects.isNull(description) ? "" : description)
+                .startDate(Objects.isNull(startDate) ? LocalDate.now() : startDate)
+                .endDate(Objects.isNull(endDate) ? LocalDate.now().plusYears(1) : endDate)
                 .imageUrl(imageUrl)
                 .member(member)
                 .build();
