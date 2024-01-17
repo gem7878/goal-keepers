80fd37b1823a97a5e27a3413da65a8dbd74516e9
pieceofjc <pieceofjc@gmail.com>
2024-01-17 18:51:02 +0900

    Fix: warnning 지움

--- server/src/main/java/com/goalkeepers/server/config/FirebaseConfig.java
+++ server/src/main/java/com/goalkeepers/server/config/FirebaseConfig.java
@@ -1,13 +1,11 @@
 package com.goalkeepers.server.config;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
--- server/src/main/java/com/goalkeepers/server/controller/CommonController.java
+++ server/src/main/java/com/goalkeepers/server/controller/CommonController.java
@@ -1,7 +1,5 @@
 package com.goalkeepers.server.controller;
 
-import static com.goalkeepers.server.entity.QGoal.goal;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
