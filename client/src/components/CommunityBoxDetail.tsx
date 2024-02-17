3258cd17485bab27c8f1e4c8b9dc3bbfef92b832
dev-minjeong <vitaminjeong1026@gmail.com>
2024-02-17 14:20:31 +0900

    보드 api 적용 진행 중

--- client/src/components/CommunityBoxDetail.tsx
+++ client/src/components/CommunityBoxDetail.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Image1 from '../../public/assets/images/goalKeepers.png';
+import Image from 'next/image';
+
 interface communityContentList {
   nickname: string;
   content: number;
@@ -21,13 +24,78 @@ interface communityContentTypes {
   count: null | number;
   share: boolean;
 }
-const CommunityBoxDetail: React.FC<{ data: communityContentTypes }> = ({
+const CommunityBoxDetail: React.FC<{ data: communityContentTypes; }> = ({
   data,
 }) => {
   return (
-    <div>
-      <h2>커뮤니티박스 디테일</h2>
-    </div>
+    <article
+      className="h-4/5
+      flex
+      justify-between
+      p-3
+      mb-4
+      border
+      rounded-md
+      duration-100
+      w-10/12
+      inset-x-0
+      mx-auto
+       "
+    >
+      <div className="w-full relative">
+        <Image
+          src={
+            data.originalGoalImageUrl === null
+              ? Image1
+              : data.originalGoalImageUrl
+          }
+          alt=""
+          fill
+          style={{
+            objectFit: 'cover',
+            zIndex: 1,
+          }}
+        ></Image>
+        <div className="bg-black opacity-40 z-10 w-full h-full absolute"></div>
+        <div className="absolute z-20 w-full h-full p-3 flex flex-col">
+          <section className="h-1/6">
+            <h3 className="text-white text-xl">{data.originalGoalTitle}</h3>
+          </section>
+          <section className="h-4/6">
+            <ul className="w-full">
+              {data.contentList.map((list, index) => {
+                return (
+                  <li
+                    key={index}
+                    className={`text-gray-600 bg-orange-100 mt-3 py-1 rounded px-2 drop-shadow-lg`}
+                  >
+                    <div>
+                      <h4 className="text-sm font-bold">{list.content}</h4>
+                    </div>
+
+                    <div>
+                      <div className="rounded-full h-5 bg-gray-600 px-2 inline-block">
+                        <p className="text-orange-300 text-[11px] leading-5 inline-block">
+                          {list.nickname}
+                        </p>
+                      </div>
+                      <span>{list.updatedAt}</span>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+          <section className="h-1/6">
+            <ul>
+              {data.joinMemberList.map((list, index) => {
+                return <li key={index}>{list}</li>;
+              })}
+            </ul>
+          </section>
+        </div>
+      </div>
+    </article>
   );
 };
 
