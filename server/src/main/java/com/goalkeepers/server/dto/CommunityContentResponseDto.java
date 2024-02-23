f50c968f30c948bbcff7c822bdae8b57d0e7c92a
pieceofjc <pieceofjc@gmail.com>
2024-02-23 23:49:23 +0900

    Fix: 포스트, 커뮤니티 메뉴에서 컨텐트 가져올 때 비공개 컨텐트 제외

--- server/src/main/java/com/goalkeepers/server/dto/CommunityContentResponseDto.java
+++ server/src/main/java/com/goalkeepers/server/dto/CommunityContentResponseDto.java
@@ -1,6 +1,9 @@
 package com.goalkeepers.server.dto;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.goalkeepers.server.entity.Goal;
 import com.goalkeepers.server.entity.PostContent;
 
 import lombok.AllArgsConstructor;
@@ -12,22 +15,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class PostContentResponseDto {
+public class CommunityContentResponseDto {
     private long contentId;
     private String nickname; // Member
     private String content; // PostContent
     private LocalDateTime createdAt; // PostContent
     private int likeCnt; // PostContent
     private boolean isLike; // PostContent, Member
+    private Long goalId; // Goal : share goal id
+    private String goalTitle; // Goal : share goal title
+    private String goalDescription; // Goal : share goal description
+    private String goalImageUrl; // Goal : share goal image_url
 
-    public static PostContentResponseDto of(PostContent postContent, String nickname, boolean isLike) {
-        return PostContentResponseDto.builder()
+    public static CommunityContentResponseDto of(PostContent postContent, Goal goal, String nickname, boolean isLike, String imageUrl) {
+        boolean isGoal = Objects.nonNull(goal);
+        return CommunityContentResponseDto.builder()
             .contentId(postContent.getId())
             .nickname(nickname)
             .content(postContent.getContent())
             .createdAt(postContent.getCreatedAt())
             .likeCnt(postContent.getLikeCnt())
             .isLike(isLike)
+            .goalId(isGoal ? goal.getId() : null)
+            .goalTitle(isGoal ? goal.getTitle() : null)
+            .goalDescription(isGoal ? goal.getDescription() : null)
+            .goalImageUrl(imageUrl)
             .build();
     }
 }
