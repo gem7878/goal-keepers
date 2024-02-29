8f2efec64b0382bf3383e5d787d1c6a01c3d5561
pieceofjc <pieceofjc@gmail.com>
2024-02-29 17:40:36 +0900

    Fix: Patch readAlarms -> Put readAlarms

--- server/src/main/java/com/goalkeepers/server/controller/NotificationController.java
+++ server/src/main/java/com/goalkeepers/server/controller/NotificationController.java
@@ -7,8 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,7 +42,7 @@ public class NotificationController {
         return ResponseEntity.ok(new CommonResponseDto(true, response));
     }
 
-    @PatchMapping(value = "/alarm")
+    @PutMapping(value = "/alarm")
     public ResponseEntity<CommonResponseDto> readAlarms() {
         Long number = notificationService.ChangeReadAlarms();
         return ResponseEntity.ok(new CommonResponseDto(true, number + "개의 알람이 모두 읽음 처리되었습니다."));
