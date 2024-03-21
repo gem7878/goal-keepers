c85d967bf12eb03c251ac4d925566ec5a36e2e8e
pieceofjc <pieceofjc@gmail.com>
2024-03-21 17:21:05 +0900

    Fix: setupProxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,7 +5,16 @@ module.exports = function (app) {
     createProxyMiddleware('/', {
       target: process.env.NEXT_PUBLIC_API_URL,
       changeOrigin: true,
-      secure: false
     }),
+  );
+
+  // '/subscribe' 경로에 대한 프록시 설정(HTTPS)
+  app.use(
+    '/subscribe',
+    createProxyMiddleware({
+      target: process.env.NEXT_PUBLIC_API_URL_HTTPS, // HTTPS 프로토콜로 대상 서버 설정
+      changeOrigin: true, // 대상 서버의 주소를 변경하여 프록시 요청을 보냄
+      secure: false // HTTPS 프로토콜을 사용하도록 설정
+    })
   );
 };
